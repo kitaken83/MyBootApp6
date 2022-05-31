7488e9b172e58a7e05dc2ef7d9f7691adb63b42e
190735 <190735@e0601-10.JC-21.local>
2022-05-31 11:35:11 +0900

    コミット2

--- src/main/java/jp/te4a/spring/boot/myapp7/MyBootApp7Application.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/MyBootApp7Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyBootApp6Application {
+public class MyBootApp7Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyBootApp6Application.class, args);
+		SpringApplication.run(MyBootApp7Application.class, args);
 	}
 
 }
